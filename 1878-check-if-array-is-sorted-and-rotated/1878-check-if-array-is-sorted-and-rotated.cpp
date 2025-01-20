f17cbe84e6990fa4a855a7dc1a543834375aaf3a
BHARGAV REDDY <112396057+bhargavbijjam@users.noreply.github.com>
2025-01-20 21:56:04 +0530

    Time: 0 ms (100%), Space: 11.2 MB (21.27%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -5,8 +5,8 @@ public:
         for(int i=1;i<nums.size();i++){
             if(nums[i-1]>nums[i]) count++;
         }
-        if(nums[nums.size()-1]>nums[0]) count++;
+        if(nums[nums.size()-1] > nums[0]) count++;
         if(count>1) return false;
-        else return true;
+        else return true;    
     }
 };
