c1fe08ed4a23266468324e83cc25eb3a7b8c083c
BHARGAV REDDY <112396057+bhargavbijjam@users.noreply.github.com>
2025-04-28 11:10:58 +0530

    Time: 0 ms (100%), Space: 12.9 MB (33.26%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    int nextGreater(int num,int j,vector<int> nums2){
-        for(int k=j+1;k<nums2.size();k++){
-            if(num<nums2[k]) return nums2[k];
+    map<int,int> nge(vector<int> nums){
+        map<int,int> store;
+        stack<int> m_stack;
+        for(int i=nums.size()-1;i>=0;i--){
+            while(!m_stack.empty() and m_stack.top()<=nums[i]){
+                m_stack.pop();
+            }
+            if(!m_stack.empty()){
+                store[nums[i]]=m_stack.top();
+            }
+            else {
+                store[nums[i]]=-1;
+            }
+            m_stack.push(nums[i]);
         }
-        return -1;
+        return store;
     }
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> ngeData = nge(nums2);
         vector<int> res;
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    res.push_back(nextGreater(nums2[j],j,nums2));
-                    break;
-                }
-            }
+        for(int i:nums1){
+            res.push_back(ngeData[i]);
         }
         return res;
     }
