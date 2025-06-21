e8b83dbf267b802a93900b7064bd1a60d4643ace
BHARGAV REDDY <112396057+bhargavbijjam@users.noreply.github.com>
2025-06-21 22:10:20 +0530

    Time: 97 ms (100%), Space: 12.9 MB (66.67%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.py
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.py
@@ -4,17 +4,15 @@ class Solution(object):
         for char in word:
             data[char]+=1
         val = data.values()
-        val.sort()
-        bef_num = 0
         res = len(word)
         for i in range(len(val)):
             deleted_chars = 0
-            for j in range(i+1,len(val)):
-                if(val[j]>val[i]+k):
+            for j in range(len(val)):
+                if(val[i]>val[j]):
+                    deleted_chars += val[j]
+                elif(val[j]>val[i]+k):
                     deleted_chars += val[j]-(val[i]+k)
-            deleted_chars += bef_num
             res = min(res,deleted_chars)
-            bef_num += val[i]
         return res
 
 
