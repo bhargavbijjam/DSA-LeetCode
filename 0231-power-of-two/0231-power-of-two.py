ad9485e0ef6038bacdbb81a302feba81b0ed670d
BHARGAV REDDY <112396057+bhargavbijjam@users.noreply.github.com>
2025-06-27 22:00:09 +0530

    Time: 2 ms (21.86%), Space: 12.4 MB (79.91%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,16 +1,6 @@
 class Solution(object):
-    def helper(self,n,count):
-        if (n<0): return False
-        if (n==0 and count==1):
-            return True
-        if(n==0):
-            return False
-        if((n & 1)==1):
-            count+=1
-        return self.helper(n>>1,count)
     def isPowerOfTwo(self, n):
-        return self.helper(n,0)
-        
+        return n>0 and (n & (n-1)==0)
         """
         :type n: int
         :rtype: bool
