a88420929ec2c95e85561d084d221e964272693e
BHARGAV REDDY <112396057+bhargavbijjam@users.noreply.github.com>
2025-07-10 13:22:04 +0530

    Time: 7050 ms (5.01%), Space: 34.7 MB (22.87%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,20 +1,28 @@
 class Solution(object):
-    def solve(self,text1,text2,i,j,dp):
-        if(i == -1 or j == -1):
-            return 0
-        if(dp[i+1][j+1]!= -1):
-            return dp[i+1][j+1]
-        if(text1[i]==text2[j]):
-            dp[i+1][j+1] = 1+self.solve(text1,text2,i-1,j-1,dp)
-        else:
-            dp[i+1][j+1] = max(self.solve(text1,text2,i-1,j,dp),self.solve(text1,text2,i,j-1,dp))
-        return  dp[i+1][j+1]
+    # def solve(self,text1,text2,i,j,dp):
+    #     if(i == -1 or j == -1):
+    #         return 0
+    #     if(dp[i+1][j+1]!= -1):
+    #         return dp[i+1][j+1]
+    #     if(text1[i]==text2[j]):
+    #         dp[i+1][j+1] = 1+self.solve(text1,text2,i-1,j-1,dp)
+    #     else:
+    #         dp[i+1][j+1] = max(self.solve(text1,text2,i-1,j,dp),self.solve(text1,text2,i,j-1,dp))
+    #     return  dp[i+1][j+1]
 
     def longestCommonSubsequence(self, text1, text2):
         n = len(text1)
         m = len(text2)
-        dp = [[-1 for _ in range(m+1)] for _ in range(n+1)]
-        return self.solve(text1,text2,n-1,m-1,dp)
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if(text1[i-1]==text2[j-1]):
+                    print(text1[i-1],text2[j-1])
+                    dp[i][j] = dp[i-1][j-1]+1
+                else:
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        return dp[n][m]
         
         """
         :type text1: str
